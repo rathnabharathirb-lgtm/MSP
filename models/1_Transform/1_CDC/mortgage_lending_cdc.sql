a440ba6528b7b4a7a0032dd0fbb7f18b12b1f41b
Rathna Bharathi <rathnabharathi.rb@gmail.com>
2025-12-11 01:22:22 +0530

    incremental

--- models/1_Transform/1_CDC/mortgage_lending_cdc.sql
+++ models/1_Transform/1_CDC/mortgage_lending_cdc.sql
@@ -1,2 +1,20 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='id',
+    on_schema_change='fail',
+    incremental_strategy='merge'
+  )
+}}
+
 select *
 from {{ ref('Mortgage_Lending') }}
+
+{% if execute %}
+  {% if flags.FULL_REFRESH %}
+    -- Full refresh: process all records
+  {% else %}
+    -- Incremental: only process records updated after last run
+    where updated_at > (select max(updated_at) from {{ this }})
+  {% endif %}
+{% endif %}
